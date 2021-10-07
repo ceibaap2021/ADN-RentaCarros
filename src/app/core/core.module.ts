5e9b8a2146b6b2cc0da510e34cbf86c2d9817014
Anderson José Pérez Beltrán- Ceiba Software <anderson.perez@ceiba.com.co>
2021-10-07 11:05:05 -0500

    fix test

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,11 @@
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SecurityGuard } from './guard/security.guard';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { TokenInterceptor } from './interceptor/token-interceptor';
-import { AuthInterceptor } from './interceptor/auth-interceptor';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HttpService } from './services/http.service';
 import { RouterModule } from '@angular/router';
+import { ManejadorError } from './interceptor/manejador-error';
 
 @NgModule({
   declarations: [ToolbarComponent, NavbarComponent],
@@ -19,8 +17,7 @@ import { RouterModule } from '@angular/router';
   providers: [
     HttpService,
     SecurityGuard,
-    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: ErrorHandler, useClass: ManejadorError },
   ]
 })
 export class CoreModule { }
