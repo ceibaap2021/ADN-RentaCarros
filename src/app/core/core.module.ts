7f9fd47eb965ef46e30bd446390561fba8b2a498
Anderson José Pérez Beltrán- Ceiba Software <anderson.perez@ceiba.com.co>
2021-10-11 13:34:10 -0500

    pruebas unitarias

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,9 +6,6 @@ import { HttpService } from './services/http.service';
 import { RouterModule } from '@angular/router';
 import { ManejadorError } from './interceptor/manejador-error';
 import { SecurityGuard } from './guard/security.guard';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from './interceptor/auth-interceptor';
-import { TokenInterceptor } from './interceptor/token-interceptor';
 
 @NgModule({
   declarations: [ToolbarComponent, NavbarComponent],
@@ -20,8 +17,6 @@ import { TokenInterceptor } from './interceptor/token-interceptor';
   providers: [
     HttpService,
     SecurityGuard,
-    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     { provide: ErrorHandler, useClass: ManejadorError }
   ]
 })
